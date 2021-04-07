64759ab17424c4b109c5c43e0e9f31cfee875064
Benjamin Hammack <bjhammack@protonmail.com>
2021-04-07 08:15:47 -0500

    Updated code for live server

--- baseball_spider.py
+++ baseball_spider.py
@@ -384,6 +384,7 @@ __{team['team']}__
 		#options.add_argument('--incognito')
 		options.add_argument('--headless')
 		options.add_argument("--log-level=3")
+		options.add_argument("--remote-debugging-port=9222")
 		# Deprecated version of creating the driver. This is the preferred approach, but does not work with 32-bit linux OS's
 		driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),options=options)
 		#driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=options)
